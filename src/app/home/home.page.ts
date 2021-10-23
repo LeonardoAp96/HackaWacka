6217e04be5e3dbd51fe6903ebe5f120f5f43a2d9
Leonardo Aparecido <contato.leonardoap@gmail.com>
2021-10-23 19:56:08 -0300

    Logica de abertura e fechamento de bloqueio

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { getCurrencySymbol } from '@angular/common';
 import { Component } from '@angular/core';
+import { platform } from 'process';
 export interface Bloqueio{
   Numero: number,
   Condicao: Boolean
@@ -16,31 +17,34 @@ export interface Plataforma{
   styleUrls: ['home.page.scss'],
 })
 
-
 export class HomePage {
 
-  private TIME_INTERVAL : number = 5000; //milisegundos
-
+  private TIME_INTERVAL : number = 3000; //milisegundos
+  routine = setInterval(()=>this.rotina(), this.TIME_INTERVAL);
   public linhaBloqueio: Bloqueio[];
   public Plataformas: Plataforma[];
+  public showData: String = "Oi";
+  public noTrem: Boolean;
+  public coefPassageiro: number;
+
   private dataInicial: Date;
   private dataAtual: Date;
+  private countSimultationTime: number = 0;
   
-  routine = setInterval(this.rotina, this.TIME_INTERVAL);
 
   constructor() {
-    this.dataInicial = new Date();  
-    console.log("Date = " + this.dataInicial); 
-
-    //Date = Tue Feb 05 2019 12:05:22 GMT+0530 (IST)  
+    this.dataInicial = this.dataAtual = new Date();  
+    this.coefPassageiro = 5;
+    this.ShowDataRotina();
     this.montarPlataforma();
   }
   
+
   private montarPlataforma(){
     this.Plataformas = [
       { Nome:"1", 
-        ContagemNorte:0.734, 
-        ContagemSul:0.35, 
+        ContagemNorte:0.1, 
+        ContagemSul:0.1, 
         LinhaBloqueio:  [ {Numero:1,Condicao: true},
                           {Numero:2,Condicao: true},
                           {Numero:3,Condicao: true},
@@ -53,21 +57,148 @@ export class HomePage {
       { Nome:"2", 
         ContagemNorte:0.2, 
         ContagemSul:0.2,
-      },
-      { Nome:"Terminal", 
-        ContagemNorte:0.652, 
-        ContagemSul:0.408
+        LinhaBloqueio: []
       }
     ];
   }
-
-  public rotina(){
+  
+  private rotina(){
     this.dataAtual = new Date();
+    this.ShowDataRotina();
+    this.Simulação();
+    this.countSimultationTime += 1;
   }
 
+  private Simulação(){
+    let countBloqueio = 0;
+    let percentualPasgPlatNorte = 0;
+    let percentualPasgPlatSul = 0;
+
+    if(this.countSimultationTime%10 == 0){ //Passagem do trem
+      for(let count = 0; count < this.Plataformas.length; count++){
+        percentualPasgPlatNorte = this.Plataformas[count].ContagemNorte *0.1;
+        percentualPasgPlatSul = this.Plataformas[count].ContagemNorte *0.1;
+        
+        this.Plataformas[count].ContagemNorte = this.setMinValue(percentualPasgPlatNorte);
+        this.Plataformas[count].ContagemSul = this.setMinValue(percentualPasgPlatSul);
+      }
+    }
+    else { //Entrada de passageiros
+      countBloqueio = this.getQuantBloqueioLigado(this.Plataformas);
+
+      for(let count = 0; count < this.Plataformas.length; count++){
+        percentualPasgPlatNorte = this.Plataformas[count].ContagemNorte + this.getRandomPassageiro(countBloqueio);
+        percentualPasgPlatSul = this.Plataformas[count].ContagemSul + this.getRandomPassageiro(countBloqueio);
+        
+        this.Plataformas[count].ContagemNorte = this.setMaxValue(this.Plataformas[count].ContagemNorte + this.getRandomPassageiro(countBloqueio));
+        this.Plataformas[count].ContagemSul =  this.setMaxValue(this.Plataformas[count].ContagemSul + this.getRandomPassageiro(countBloqueio));
+      };
+    };
+    this.AlgoritmoBloqueios();
+  }
+
+  private AlgoritmoBloqueios(){
+    let somaOcupacao = 0;
+    let qtdPlat = 0;
+
+    this.Plataformas.forEach(pl=>{
+      somaOcupacao += (pl.ContagemNorte + pl.ContagemNorte)/2;
+      qtdPlat++;
+    });
+
+    
+    const mediaOcupacao = somaOcupacao / qtdPlat;
+    console.log(somaOcupacao, qtdPlat,  mediaOcupacao)
+
+    if(mediaOcupacao <.5){
+      this.setLigarBloqueio(4);
+    }
+    if(mediaOcupacao >.5){
+      this.setLigarBloqueio(3);
+    }
+    if(mediaOcupacao >.7){
+      this.setLigarBloqueio(2);
+    }
+    if(mediaOcupacao >.8){
+      this.setLigarBloqueio(1);
+    }
+
+  }
+
+//Funcoes
+
+  private setLigarBloqueio(number: Number){
+    
+    for (let index = 0; index < this.Plataformas[0].LinhaBloqueio.length; index++) {
+      this.Plataformas[0].LinhaBloqueio[index].Condicao = index < number? true:false;
+    }
+  }
+
+  private getRandomPassageiro(count:number) {
+    return ((Math.random() * count) / 100) * this.coefPassageiro;
+  }
+
+  private ShowDataRotina(){
+    this.showData = this.dataAtual.getDay() + "/" + 
+                    this.dataAtual.getMonth()  + "/" + 
+                    this.dataAtual.getFullYear() + "  " + 
+                    this.dataAtual.getHours() + ":" +
+                    this.dataAtual.getMinutes() + ":" +
+                    this.dataAtual.getSeconds();
+    
+  }
+
+  private getQuantBloqueioLigado(plataformas :Plataforma[]){
+    let variavel = 0;
+    plataformas.forEach(lb => {
+      let linhabloqueio = lb.LinhaBloqueio.length;
+      if(linhabloqueio) {
+        for (let index = 0; index < linhabloqueio; index++) {
+           if(lb.LinhaBloqueio[index].Condicao) variavel++;
+        }
+      }
+    });
+    return variavel;
+  }
+
+  private setMinValue(val: number){
+    return val <0?0:val;
+  }
+
+  private setMaxValue(val: number){
+    return val >1?1:val;
+  }
 
   public setCondicao(cond: Boolean){
     return cond ? "Ligado" : "Desligado";
+  }
+
+  public setCondicaoArrow(cond: number){
+    return cond <.5 ? "Ligado" : "Desligado";
+  }
+
+  public setCondicaoArrowNorte(cond: Plataforma){
+    const condicao = cond.ContagemNorte;
+    
+    if(condicao < .5)
+      return "Ligado";
+    
+    if(cond.ContagemSul > condicao)
+      return "Ligado";
+    
+    return "Desligado";
+  }
+
+  public setCondicaoArrowSul(cond: Plataforma){
+    const condicao = cond.ContagemSul;
+    
+    if(condicao < .5)
+      return "Ligado";
+    
+    if(cond.ContagemNorte > condicao)
+      return "Ligado";
+    
+    return "Desligado";
   }
 
   public setPercentual(numero: number){
