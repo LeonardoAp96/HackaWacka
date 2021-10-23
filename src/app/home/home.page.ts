95ec54cb5879351f3fd8de93e9bc6049b63d40c1
Leonardo Aparecido <contato.leonardoap@gmail.com>
2021-10-23 12:30:21 -0300

    Linha de Bloqueios

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,45 @@
+import { getCurrencySymbol } from '@angular/common';
 import { Component } from '@angular/core';
-
+export interface Bloqueio{
+  Numero: number,
+  Condicao: Boolean
+}
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
+
 export class HomePage {
 
-  constructor() {}
+  public linhaBloqueio: Bloqueio[];
+
+
+  constructor() {
+    this.linhaBloqueio = [
+      {Numero:1,Condicao: true},
+      {Numero:2,Condicao: false},
+      {Numero:3,Condicao: true},
+      {Numero:4,Condicao: true},
+      {Numero:5,Condicao: true},
+      {Numero:5,Condicao: true},
+    ];
+  }
+
+  public setCondicao(cond: Boolean){
+    return cond ? "Ligado" : "Desligado";
+  }
+
+
+
+
+
+
+
+
+
+
 
 }
