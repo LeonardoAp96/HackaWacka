bf45de33f86a859f3474189673636665dd9639d1
Leonardo Aparecido <contato.leonardoap@gmail.com>
2021-10-23 14:37:55 -0300

    Plataformas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,12 @@ export interface Bloqueio{
   Numero: number,
   Condicao: Boolean
 }
+export interface Plataforma{
+  Nome: String,
+  ContagemSul: number,
+  ContagemNorte: number,
+  LinhaBloqueio?: Bloqueio[]
+}
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -11,20 +17,34 @@ export interface Bloqueio{
 })
 
 
-
 export class HomePage {
 
   public linhaBloqueio: Bloqueio[];
+  public Plataformas: Plataforma[];
 
 
   constructor() {
-    this.linhaBloqueio = [
-      {Numero:1,Condicao: true},
-      {Numero:2,Condicao: false},
-      {Numero:3,Condicao: true},
-      {Numero:4,Condicao: true},
-      {Numero:5,Condicao: true},
-      {Numero:5,Condicao: true},
+    this.Plataformas = [
+      { Nome:"1", 
+        ContagemNorte:0.734, 
+        ContagemSul:0.35, 
+        LinhaBloqueio:  [ {Numero:1,Condicao: true},
+                          {Numero:2,Condicao: false},
+                          {Numero:3,Condicao: true},
+                          {Numero:4,Condicao: false},
+                          {Numero:5,Condicao: true},
+                          {Numero:6,Condicao: true},
+                          {Numero:7,Condicao: false},
+                          {Numero:8,Condicao: true}],
+      },
+      {Nome:"2", 
+      ContagemNorte:0.2, 
+      ContagemSul:0.2, 
+      LinhaBloqueio:  [ {Numero:1,Condicao: true},
+                        {Numero:2,Condicao: false},
+                        {Numero:3,Condicao: true}],
+      },
+      {Nome:"Terminal", ContagemNorte:0.652, ContagemSul:0.408}
     ];
   }
 
@@ -32,14 +52,10 @@ export class HomePage {
     return cond ? "Ligado" : "Desligado";
   }
 
-
-
-
-
-
-
-
-
+  public setPercentual(numero: number){
+    let num = numero * 100;
+    return num.toFixed(2);
+  }
 
 
 }
